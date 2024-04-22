3145d6dcdbd6e7f0eed325f30c9aa2ad6c4f2b59
Richard Lynch <rlynch2012@gmail.com>
2024-04-22 01:04:00 -0400

    add component and props for search-box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import CardList from "./components/card-list/search-box/card-list.component";
+import SearchBox from "./components/card-list/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -16,11 +17,9 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
@@ -32,6 +31,7 @@ class App extends Component {
   };
 
   render() {
+    // console.log('render from AppJS');
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster) => {
@@ -39,22 +39,12 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          name="name"
-          autoComplete="false"
+        <SearchBox
+          onChangeHandler={onSearchChange}
           placeholder="search monsters"
-          onChange={onSearchChange}
+          className="search-box"
         />
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
-        <CardList monsters = {filteredMonsters}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
