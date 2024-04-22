796f97b7fba9ef78f5eb207250b5cad3cb024fc8
Richard Lynch <rlynch2012@gmail.com>
2024-04-21 22:45:03 -0400

    add props to component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
+import CardList from "./components/card-list/search-box/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -11,20 +10,15 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("componentDidMound");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
         this.setState(
           () => {
             return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
           }
         )
       );
@@ -38,8 +32,6 @@ class App extends Component {
   };
 
   render() {
-    console.log("render");
-
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster) => {
@@ -50,16 +42,19 @@ class App extends Component {
         <input
           className="search-box"
           type="search"
+          name="name"
+          autoComplete="false"
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList monsters = {filteredMonsters}/>
       </div>
     );
   }
