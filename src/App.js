08c02061668001f98187bed38d517a0e1ab62f1b
Richard Lynch <rlynch2012@gmail.com>
2024-05-03 01:47:41 -0400

    change app to functionial component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import { Component } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 const App = () => {
-  const [searchField, setSearchField] = useState('');
-  console.log(searchField);
+  console.log("render");
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
@@ -19,7 +37,7 @@ const App = () => {
         placeholder="search monsters"
         className="monsters-search-box"
       />
-      {/* <CardList monsters={filteredMonsters} />  */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
